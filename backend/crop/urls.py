18d10cac22fb65f96e16f695d7f77a3178d2b4f9
siamliam12 <129056535+siamliam12@users.noreply.github.com>
2024-10-05 04:55:22 +0600

    deploy

--- backend/crop/urls.py
+++ backend/crop/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_power_data,get_form_data,predict_crop,predict_crop_yield
+from .views import get_power_data,get_form_data,predict_crop,predict_crop_yield,root
 
 urlpatterns =[
+    path('', root, name='root'),
     path('get-power-data', get_power_data, name='get_power_data'),
     path('get-form-data', get_form_data, name='get_form_data'),
     path('recommend-crop', predict_crop, name='predict_crop'),
--- backend/crop/yeilddatannnnn.py
+++ backend/crop/yeilddatannnnn.py
@@ -1,8 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pickle
 import json
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
