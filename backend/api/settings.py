e50d05aff4ca046ea78912e13a7fd9bd375792ec
siamliam12 <129056535+siamliam12@users.noreply.github.com>
2024-10-05 07:50:02 +0600

    k

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = "api.wsgi.application"
 DATABASES = {
    'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://siamliam12:fHghsCAuWgJ35rPLteLYTdO2RSXRSzoY@dpg-cs04kjl2ng1s73em02sg-a.singapore-postgres.render.com/backendserver_lwxw',
+        default='postgresql://siamliam12:ZHYDVs64chrhaTRmnbrAP44gZtxi7Ipb@dpg-cs09fk8gph6c73a4l4g0-a.singapore-postgres.render.com/server_wbon',
         conn_max_age=600
     )
 }
